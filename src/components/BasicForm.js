a48f6961792ebda47bc8c86d0df0c416db48a509
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-02-28 10:59:22 +0530

    conditional statements rearranged

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -55,19 +55,22 @@ const BasicForm = (props) => {
         <div className={firstnameInputClasses}>
           <label htmlFor='firstname'>First Name</label>
           <input type='text' id='firstname' onChange={firstnameChangeHandler} onBlur={firstnameBlurHandler} />
+          {firstnameInputHasError && <p className="error-text">Firstname must not be empty.</p>}
         </div>
-        {firstnameInputHasError && <p className="error-text">Firstname must not be empty.</p>}
+        
         <div className={lastnameInputClasses}>
           <label htmlFor='lastname'>Last Name</label>
           <input type='text' id='lastname' onChange={lastnameChangeHandler} onBlur={lastnameBlurHandler} />
+          {lastnameInputHasError && <p className="error-text">Lastname must not be empty.</p>}
         </div>
-        {lastnameInputHasError && <p className="error-text">Lastname must not be empty.</p>}
+        
       </div>
       <div className={emailInputClasses}>
         <label htmlFor='email'>E-Mail Address</label>
         <input type='text' id='email' onChange={emailChangeHandler} onBlur={emailBlurHandler}/>
+        {emailInputHasError && <p className="error-text">Email must not be emptyand it must contain '@'</p>}
       </div>
-      {emailInputHasError && <p className="error-text">Email must not be emptyand it must contain '@'</p>}
+      
       <div className='form-actions'>
         <button disabled={!formIsValid}>Submit</button>
       </div>
